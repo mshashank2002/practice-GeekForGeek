58ef9e5527686e8fa66ef3bb1e9f4e17394265b2
mshashank2002 <mshashank8867@gmail.com>
2022-10-13 21:55:47 +0530

    solution added

---
+++ TomAndJerry.java
@@ -0,0 +1,15 @@
+public class TomAndJerry
+{
+    static int numsGame(int N)
+    {
+        if(N%2==0)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+
+    }
+}
